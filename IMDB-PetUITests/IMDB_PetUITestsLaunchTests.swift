3ca278f6ab814a2d66e00fc9159fced32805298c
Henadzi <henadzibahdanovich@gmail.com>
2022-11-29 11:55:15 +0100

    Initial Commit

---
+++ IMDB-PetUITests/IMDB_PetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IMDB_PetUITestsLaunchTests.swift
+//  IMDB-PetUITests
+//
+//  Created by Henadzi on 29/11/2022.
+//
+
+import XCTest
+
+final class IMDB_PetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
